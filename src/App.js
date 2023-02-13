662f9d9c12c9c092ba86d8692af37767799a2a5b
kumomiki <rita21625@gmail.com>
2023-02-13 14:03:46 -0500

    feat: add StepProgress.js and StepProgress.module.scss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { StepOne } from "./components/main/progress/stepForms/StepOne.js";
 import { StepThree } from "./components/main/progress/stepForms/StepThree.js";
 import { StepTwo } from "./components/main/progress/stepForms/StepTwo.js";
+import { StepProgress } from "./components/main/progress/StepProgress.js";
 
 
 function App() {
   return (
     <div className="App">
+      <StepProgress/>
       <StepOne />
       <StepTwo />
       <StepThree />
---
+++ src/components/main/progress/StepProgress.js
@@ -0,0 +1,46 @@
+import styles from './StepProgress.module.scss'
+import { ReactComponent as Check} from '../../../icons/Check.svg'
+
+function Step({step, currentStep}){
+  return (
+    <div className={styles.progressIconGroups}>
+      <div class={currentStep >= step? styles.progressIconDone : styles.progressIconUndone}>
+        <div class={styles.progressIconText}>{currentStep >= step ? <Check />: step}</div>
+      </div>
+    </div>
+  )
+}
+
+
+function ProgressLine({className}){
+  return (
+    <div className={className}></div>
+  )
+}
+
+export function StepProgress({currentStep}){
+  return (
+    <section className={styles.stepProgressContainer}>
+      <h2 className={styles.stepProgressTitle}>結帳</h2>
+      <div className={styles.stepProgressWrapper}>
+        <Step
+        currentStep={currentStep}
+        step={1}
+        />
+        <ProgressLine 
+        className={styles.leftProgressLine} 
+        />
+        <Step
+        currentStep={currentStep}
+        step={2}
+        />
+        <ProgressLine className={`${currentStep >= 2 ? styles.progressLineDone : styles.progressIconUndone} ${styles.rightProgressLine}`}
+        />
+        <Step
+        currentStep={currentStep}
+        step={3}
+        />
+      </div>
+    </section>
+  )
+}
