7a44e095d0f5bdc3b98f9f8072a8cfae37bd17e2
kumomiki <rita21625@gmail.com>
2023-02-13 09:08:47 -0500

    feat: add StepTwo.js and StepTwo.module.scss

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
-import { RadioGroups } from "./components/main/progress/stepForms/elementGroups/RadioGroups.js";
 import { StepOne } from "./components/main/progress/stepForms/StepOne.js";
 import { StepThree } from "./components/main/progress/stepForms/StepThree.js";
+import { StepTwo } from "./components/main/progress/stepForms/StepTwo.js";
 
 
 function App() {
   return (
     <div className="App">
       <StepOne />
-      <RadioGroups 
-      label='標準運送'
-      fee='0'
-      price='免費'
-      period='約3~7個工作天'
-      id='shippingStandard'
-      />
+      <StepTwo />
       <StepThree />
     </div>
   );
---
+++ src/components/main/progress/stepForms/StepTwo.js
@@ -0,0 +1,35 @@
+import { RadioGroups } from './elementGroups/RadioGroups'
+import { Form } from './Form'
+import styles from './StepTwo.module.scss'
+
+// FormRow
+function FormRow({children, className}){
+  return (
+    <div className={`${styles.formRow} ${className}`}>{children}</div>
+  )
+}
+
+export function StepTwo(){
+  return (
+    <Form>
+      <FormRow>
+        <RadioGroups 
+        label='標準運送'
+        fee='0'
+        price='免費'
+        period='約3~7個工作天'
+        id='shippingStandard'
+      />
+      </FormRow>
+      <FormRow>
+        <RadioGroups
+        label='DHL貨運'
+        fee='500'
+        price='$500'
+        period='48小時內抵達'
+        id='shippingDHL' 
+        />
+      </FormRow>
+    </Form>
+  )
+}
