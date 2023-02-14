7da4092806991e6a59462ead516112dc1e05a88c
kumomiki <rita21625@gmail.com>
2023-02-14 09:31:32 -0500

    feat: add pop-up menu and pop-up menu css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,53 @@
-import styles from './Header.module.scss'
-import { ReactComponent as Hamburger } from '../icons/Hamburger.svg' 
-import { ReactComponent as Logo } from '../icons/Logo.svg' 
+import styles from "./Header.module.scss";
+import { ReactComponent as Hamburger } from "../icons/Hamburger.svg";
+import { ReactComponent as Logo } from "../icons/Logo.svg";
+import { ReactComponent as Search } from "../icons/Search.svg";
+import { ReactComponent as ShoppingCart } from "../icons/ShoppingCart.svg";
+import { ReactComponent as Moon } from "../icons/Moon.svg";
 
-export function Header(){
+export function Header() {
   return (
     <header class={styles.siteHeader}>
       <div class={styles.headerContainer}>
-        <Hamburger 
-        className={styles.hamburger}
-        />
-        <Logo 
-        className={styles.logo}
-        />
+        <Hamburger className={styles.hamburger} />
+        <Logo className={styles.logo} />
       </div>
+      <nav class={styles.navMenuContainer}>
+        <ul className={styles.navMenuWrapper}>
+          <li class={styles.navItem}>
+            <a class={styles.navLink} href="/">
+              男款
+            </a>
+          </li>
+          <li class={styles.navItem}>
+            <a class={styles.navLink} href="/">
+              女款
+            </a>
+          </li>
+          <li class={styles.navItem}>
+            <a class={styles.navLink} href="/">
+              最新消息
+            </a>
+          </li>
+          <li class={styles.navItem}>
+            <a class={styles.navLink} href="/">
+              客製商品
+            </a>
+          </li>
+          <li class={styles.navItem}>
+            <a class={styles.navLink} href="/">
+              聯絡我們
+            </a>
+          </li>
+          <li class={styles.navIconList}>
+            <Search />
+            <ShoppingCart 
+            className={styles.navShoppingCart}
+            />
+            <Moon />
+          </li>
+        </ul>
+      </nav>
     </header>
-  )
+  );
 }
