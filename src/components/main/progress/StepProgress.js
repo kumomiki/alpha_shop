1d96ecb8ecac1efd7ab73a446ae4b2ecfe844bea
kumomiki <rita21625@gmail.com>
2023-02-17 17:13:01 -0600

    docs: restructure stepFrom

--- src/components/main/progress/StepProgress.js
+++ src/components/main/progress/StepProgress.js
@@ -1,58 +1,66 @@
-import styles from "./StepProgress.module.scss";
-import { ReactComponent as Check } from "../../../icons/Check.svg";
+import styles from './StepProgress.module.scss'
+import { ReactComponent as Check} from '../../../icons/Check.svg'
 
-function Step({ step, currentStep }) {
+function Step({step, currentStep}){
   return (
     <div className={styles.progressIconGroups}>
-      <div
-        class={
-          currentStep >= step
-            ? styles.progressIconDone
-            : styles.progressIconUndone
-        }
-      >
-        <div class={styles.progressIconText}>
-          {currentStep > step ? <Check /> : step}
-        </div>
+      <div class={currentStep >= step? styles.progressIconDone : styles.progressIconUndone}>
+        <div class={styles.progressIconText}>{currentStep > step ? <Check />: step}</div>
       </div>
     </div>
-  );
+  )
 }
 
-function ProgressLabel({ label }) {
-  return <div className={styles.progressLabel}>{label}</div>;
+function ProgressLabel({label}){
+  return (
+    <div className={styles.progressLabel}>{label}</div>
+  )
 }
 
-function ProgressLine({ className }) {
-  return <div className={className}></div>;
+function ProgressLine({className}){
+  return (
+    <div className={className}></div>
+  )
 }
 
-export function StepProgress({ currentStep, className }) {
+export function StepProgress({currentStep, className}){
   return (
     <section className={`${styles.stepProgressContainer} ${className}`}>
       <h2 className={styles.stepProgressTitle}>結帳</h2>
       <div className={styles.stepProgressWrapper}>
         <div className={styles.stepLabelGroups}>
-          <Step currentStep={currentStep} step={1} />
-          <ProgressLabel label="寄送地址" />
+        <Step
+        currentStep={currentStep}
+        step={1}
+        />
+        <ProgressLabel 
+        label='寄送地址'
+        />
         </div>
-        <ProgressLine className={styles.leftProgressLine} />
-        <div className={styles.stepLabelGroups}>
-          <Step currentStep={currentStep} step={2} />
-          <ProgressLabel label="運送方式" />
-        </div>
-        <ProgressLine
-          className={`${
-            currentStep >= 2
-              ? styles.progressLineDone
-              : styles.progressIconUndone
-          } ${styles.rightProgressLine}`}
+        <ProgressLine 
+        className={styles.leftProgressLine} 
         />
         <div className={styles.stepLabelGroups}>
-          <Step currentStep={currentStep} step={3} />
-          <ProgressLabel label="付款資訊" />
+        <Step
+        currentStep={currentStep}
+        step={2}
+        />
+        <ProgressLabel 
+        label='運送方式'
+        />
+        </div>
+        <ProgressLine className={`${currentStep >= 2 ? styles.progressLineDone : styles.progressIconUndone} ${styles.rightProgressLine}`}
+        />
+        <div className={styles.stepLabelGroups}>
+        <Step
+        currentStep={currentStep}
+        step={3}
+        />
+        <ProgressLabel 
+        label='付款資訊'
+        />
         </div>
       </div>
     </section>
-  );
+  )
 }
--- src/components/main/progress/stepForms/StepOne.js
+++ src/components/main/progress/stepForms/StepOne.js
@@ -1,203 +1,195 @@
-import { SelectGroups } from './elementGroups/SelectGroups.js'
-import { InputGroups } from './elementGroups/InputGroups.js'
-import styles from './StepOne.module.scss'
-import { Form } from './Form.js'
+import { SelectGroups } from "./elementGroups/SelectGroups.js";
+import { InputGroups } from "./elementGroups/InputGroups.js";
+import styles from "./StepOne.module.scss";
+import { Form } from "./Form.js";
 
 const titleOptions = [
-  {value:'mr.',
-   name:'先生'
+  { value: "mr.", name: "先生" },
+  {
+    value: "ms.",
+    name: "小姐",
   },
   {
-    value:'ms.',
-    name:'小姐'
+    value: "mx.",
+    name: "不明",
   },
-  {
-    value:'mx.',
-    name:'不明'
-  }
-]
+];
 
 const cityOptions = [
-  { name:'請選擇縣市',
-    value:''
+  { name: "請選擇縣市", value: "" },
+  {
+    name: "基隆市",
+    value: "KLU",
   },
-  { 
-    name:'基隆市',
-    value:'KLU'
+  {
+    name: "新北市",
+    value: "TPH",
   },
-  { 
-    name:'新北市',
-    value:'TPH'
+  {
+    name: "臺北市",
+    value: "TPE",
   },
-  { 
-    name:'臺北市',
-    value:'TPE'
+  {
+    name: "桃園市",
+    value: "TYC",
   },
-  { 
-    name:'桃園市',
-    value:'TYC'
+  {
+    name: "新竹縣",
+    value: "HSH",
   },
-  { 
-    name:'新竹縣',
-    value:'HSH'
+  {
+    name: "新竹市",
+    value: "HSC",
   },
-  { 
-    name:'新竹市',
-    value:'HSC'
+  {
+    name: "苗栗市",
+    value: "MAC",
   },
-  { 
-    name:'苗栗市',
-    value:'MAC'
+  {
+    name: "苗栗縣",
+    value: "MAL",
   },
-  { 
-    name:'苗栗縣',
-    value:'MAL'
+  {
+    name: "臺中市",
+    value: "TXG",
   },
-  { 
-    name:'臺中市',
-    value:'TXG'
+  {
+    name: "彰化縣",
+    value: "CWH",
   },
-  { 
-    name:'彰化縣',
-    value:'CWH'
+  {
+    name: "彰化市",
+    value: "CWS",
   },
-  { 
-    name:'彰化市',
-    value:'CWS'
+  {
+    name: "南投市",
+    value: "NTC",
   },
-  { 
-    name:'南投市',
-    value:'NTC'
+  {
+    name: "南投縣",
+    value: "NTO",
   },
-  { 
-    name:'南投縣',
-    value:'NTO'
+  {
+    name: "雲林縣",
+    value: "YLH",
   },
-  { 
-    name:'雲林縣',
-    value:'YLH'
+  {
+    name: "嘉義縣",
+    value: "CHY",
   },
-  { 
-    name:'嘉義縣',
-    value:'CHY'
+  {
+    name: "嘉義市",
+    value: "CYI",
   },
-  { 
-    name:'嘉義市',
-    value:'CYI'
+  {
+    name: "臺南市",
+    value: "TNN",
   },
-  { 
-    name:'臺南市',
-    value:'TNN'
+  {
+    name: "高雄市",
+    value: "KHH",
   },
-  { 
-    name:'高雄市',
-    value:'KHH'
+  {
+    name: "屏東縣",
+    value: "IUH",
   },
-  { 
-    name:'屏東縣',
-    value:'IUH'
+  {
+    name: "屏東市",
+    value: "PTS",
   },
-  { 
-    name:'屏東市',
-    value:'PTS'
+  {
+    name: "宜蘭縣",
+    value: "ILN",
   },
-  { 
-    name:'宜蘭縣',
-    value:'ILN'
+  {
+    name: "宜蘭市",
+    value: "ILC",
   },
-  { 
-    name:'宜蘭市',
-    value:'ILC'
+  {
+    name: "花蓮縣",
+    value: "HWA",
   },
-  { 
-    name:'花蓮縣',
-    value:'HWA'
+  {
+    name: "花蓮市",
+    value: "HWC",
   },
-  { 
-    name:'花蓮市',
-    value:'HWC'
+  {
+    name: "臺東市",
+    value: "TTC",
   },
-  { 
-    name:'臺東市',
-    value:'TTC'
+  {
+    name: "臺東縣",
+    value: "TTT",
   },
-  { 
-    name:'臺東縣',
-    value:'TTT'
+  {
+    name: "澎湖縣",
+    value: "PEH",
   },
-  { 
-    name:'澎湖縣',
-    value:'PEH'
+  {
+    name: "金門縣",
+    value: "KMN",
   },
-  { 
-    name:'金門縣',
-    value:'KMN'
+  {
+    name: "連江縣",
+    value: "LNN",
   },
-  { 
-    name:'連江縣',
-    value:'LNN'
-  }
-]
+];
 
 // FormRow
-function FormRow({children, className}){
-  return (
-    <div className={`${styles.formRow} ${className}`}>{children}</div>
-  )
+function FormRow({ children, className }) {
+  return <div className={`${styles.formRow} ${className}`}>{children}</div>;
 }
 
 export function StepOne() {
   return (
-    <Form 
-    title='寄送地址'
-    className={styles.stepOneContainer}>
-    <div className={styles.StepOneWrapper}>
-      <FormRow className={styles.formRowGrid}>
-      <SelectGroups 
-      label='稱謂'
-      options={titleOptions}
-      className={styles.titleRow}
-      />
-      <InputGroups 
-      label='姓名'
-      placeholder='請輸入姓名'
-      type='text'
-      id='name'
-      />
-      </FormRow>
-      <div className={styles.formRowGrid}>
-      <InputGroups 
-      label='電話'
-      placeholder='請輸入行動電話'
-      type='number'
-      id='phoneNumber'
-      maxLength='10'
-      />
-      {/* </FormRow>
+    <Form title="寄送地址">
+      <div className={styles.StepOneWrapper}>
+        <FormRow className={styles.formRowGrid}>
+          <SelectGroups
+            label="稱謂"
+            options={titleOptions}
+            className={styles.titleRow}
+          />
+          <InputGroups
+            label="姓名"
+            placeholder="請輸入姓名"
+            type="text"
+            id="name"
+          />
+        </FormRow>
+        <div className={styles.formRowGrid}>
+          <InputGroups
+            label="電話"
+            placeholder="請輸入行動電話"
+            type="number"
+            id="phoneNumber"
+            maxLength="10"
+          />
+          {/* </FormRow>
       <FormRow className={styles.formRowGrid}> */}
-      <InputGroups 
-      label='Email'
-      placeholder='請輸入電子郵件'
-      type='email'
-      id='Email'
-      />
-      </div>
-      <div className={styles.formRowGrid}>
-      <SelectGroups 
-      label='縣市'
-      options={cityOptions}
-      className={styles.select}
-      />
-      {/* </FormRow>
+          <InputGroups
+            label="Email"
+            placeholder="請輸入電子郵件"
+            type="email"
+            id="Email"
+          />
+        </div>
+        <div className={styles.formRowGrid}>
+          <SelectGroups
+            label="縣市"
+            options={cityOptions}
+            className={styles.select}
+          />
+          {/* </FormRow>
       <FormRow className={styles.formRowGrid}> */}
-      <InputGroups 
-      label='地址'
-      placeholder='請輸入地址'
-      type='text'
-      id='address'
-      />
+          <InputGroups
+            label="地址"
+            placeholder="請輸入地址"
+            type="text"
+            id="address"
+          />
+        </div>
       </div>
-    </div>
     </Form>
   );
 }
