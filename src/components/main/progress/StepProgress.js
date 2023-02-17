3bed87c36c4aa7f03f78aed7d4b077ce05b38d5f
kumomiki <rita21625@gmail.com>
2023-02-17 16:42:40 -0600

    docs: restructure stepProgress

--- src/components/main/progress/StepProgress.js
+++ src/components/main/progress/StepProgress.js
@@ -1,63 +1,58 @@
-import styles from './StepProgress.module.scss'
-import { ReactComponent as Check} from '../../../icons/Check.svg'
+import styles from "./StepProgress.module.scss";
+import { ReactComponent as Check } from "../../../icons/Check.svg";
 
-function Step({step, currentStep}){
+function Step({ step, currentStep }) {
   return (
     <div className={styles.progressIconGroups}>
-      <div class={currentStep >= step? styles.progressIconDone : styles.progressIconUndone}>
-        <div class={styles.progressIconText}>{currentStep > step ? <Check />: step}</div>
+      <div
+        class={
+          currentStep >= step
+            ? styles.progressIconDone
+            : styles.progressIconUndone
+        }
+      >
+        <div class={styles.progressIconText}>
+          {currentStep > step ? <Check /> : step}
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-function ProgressLabel({label, className}){
-  return (
-    <div className={`${styles.progressLabel} ${className}`}>{label}</div>
-  )
+function ProgressLabel({ label }) {
+  return <div className={styles.progressLabel}>{label}</div>;
 }
 
-function ProgressLine({className}){
-  return (
-    <div className={className}></div>
-  )
+function ProgressLine({ className }) {
+  return <div className={className}></div>;
 }
 
-export function StepProgress({currentStep, className}){
+export function StepProgress({ currentStep, className }) {
   return (
     <section className={`${styles.stepProgressContainer} ${className}`}>
       <h2 className={styles.stepProgressTitle}>結帳</h2>
       <div className={styles.stepProgressWrapper}>
-        <Step
-        currentStep={currentStep}
-        step={1}
+        <div className={styles.stepLabelGroups}>
+          <Step currentStep={currentStep} step={1} />
+          <ProgressLabel label="寄送地址" />
+        </div>
+        <ProgressLine className={styles.leftProgressLine} />
+        <div className={styles.stepLabelGroups}>
+          <Step currentStep={currentStep} step={2} />
+          <ProgressLabel label="運送方式" />
+        </div>
+        <ProgressLine
+          className={`${
+            currentStep >= 2
+              ? styles.progressLineDone
+              : styles.progressIconUndone
+          } ${styles.rightProgressLine}`}
         />
-        <ProgressLabel 
-        label='寄送地址'
-        className={styles.labelHidden}
-        />
-        <ProgressLine 
-        className={styles.leftProgressLine} 
-        />
-        <Step
-        currentStep={currentStep}
-        step={2}
-        />
-        <ProgressLabel 
-        label='運送方式'
-        className={styles.labelHidden}
-        />
-        <ProgressLine className={`${currentStep >= 2 ? styles.progressLineDone : styles.progressIconUndone} ${styles.rightProgressLine}`}
-        />
-        <Step
-        currentStep={currentStep}
-        step={3}
-        />
-        <ProgressLabel 
-        label='付款資訊'
-        className={styles.labelHidden}
-        />
+        <div className={styles.stepLabelGroups}>
+          <Step currentStep={currentStep} step={3} />
+          <ProgressLabel label="付款資訊" />
+        </div>
       </div>
     </section>
-  )
+  );
 }
