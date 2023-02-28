5ad04fec791da2f4fede11972595de75a8494dbd
kumomiki <rita21625@gmail.com>
2023-02-28 10:25:29 -0600

    fix: modify class to className

--- src/components/main/progress/stepForms/elementGroups/RadioGroups.jsx
+++ src/components/main/progress/stepForms/elementGroups/RadioGroups.jsx
@@ -3,16 +3,16 @@ import styles from './RadioGroups.module.scss'
 export function RadioGroups({label, fee, price, period, id}){
   return (
     <div className={styles.radioContainer}>
-    <label class={styles.radioGroup} data-price={fee}>
-      <input id={id} type="radio" name="shipping"  checked/>
-        <div class={styles.shippingInfo}>
-          <div class={styles.shippingFeeInfo}>
-            <div class={styles.text}>{label}</div>
-            <div class={styles.price}>{price}</div>
+      <label className={styles.radioGroup} data-price={fee}>
+        <input id={id} type="radio" name="shipping" checked />
+        <div className={styles.shippingInfo}>
+          <div className={styles.shippingFeeInfo}>
+            <div className={styles.text}>{label}</div>
+            <div className={styles.price}>{price}</div>
           </div>
-          <div class={styles.period}>{period}</div>
+          <div className={styles.period}>{period}</div>
         </div>
-    </label>
+      </label>
     </div>
-  )
+  );
 }
